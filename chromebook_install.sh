cd93626dffc3031764c1440f5aceeb62e4f2b574
Joseph Lukasik <jglukasik@wisc.edu>
2014-11-06 22:11:17 -0600

    Added chroot-making comment, and mosh

--- chromebook_install.sh
+++ chromebook_install.sh
@@ -1,1 +1,4 @@
-apt-get install vim git i3 feh tmux rxvt-unicode xfonts-terminus manpages
+# Before doing this, make the chroot with this:
+# sh -e ~/Downloads/crouton -r trusty -t core,cli-extra,audio,xorg,x11,extension,chrome-beta
+
+apt-get install vim git i3 feh tmux rxvt-unicode xfonts-terminus manpages mosh
