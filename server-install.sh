e33fb0d1778f8577a3aa160d3c761a9e9cc01cbd
Joseph Lukasik <jglukasik@wisc.edu>
2015-03-30 23:49:41 -0400

    Split into srv/client scripts, no default force

---
+++ server-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+dotfile_dir="$HOME/dotfiles"
+
+ln -s "$@" $dotfile_dir/bashrc ~/.bashrc
+ln -s "$@" $dotfile_dir/vimrc ~/.vimrc
+
+vundle="$HOME/.vim/bundle/Vundle.vim"
+if [ ! -d $vundle ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git $vundle
+fi
